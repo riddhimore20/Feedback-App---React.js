2e4c688b3a0a5e4f2163c35fe2b534d28835ae2f
riddhimore20 <66798339+riddhimore20@users.noreply.github.com>
2024-10-23 17:04:45 +0200

    Add files via upload

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 8,
+        text: "Good Product"
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Average"
+    },
+    {
+        id: 3,
+        rating: 10,
+        text: "Nice Product"
+    }
+]
+
+export default FeedbackData
